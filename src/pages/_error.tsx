6520c52f137183791f0f26f84cd115d3bc0e1626
kijimaru <eddix.ibanez@outlook.jp>
2020-03-30 01:43:29 +0900

    add error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react';
 import Head from 'next/head';
+import { NextPage, NextPageContext } from 'next';
 
 type Props = {
-  statusCode: number;
+  statusCode?: number;
 };
 
-export default ({
+const ErrorPage: NextPage<Props> = ({
   statusCode = 400,
 }: Props): React.ReactElement => {
   return (
     <>
       <Head><title>{statusCode}</title></Head>
-      <div className="ststus-code"></div>
+      <div>
+        <div className="status-code">{statusCode}</div>
+      </div>
       <style jsx>{`
         .status-code {
-          font-size: 40px;
+          margin-top: 10rem;
+          font-size: 20rem;
+          text-align: center;
         }
       `}</style>
-    </>  
+    </>
   );
 };
+
+ErrorPage.getInitialProps = ({ res, err }: NextPageContext): Props => {
+  return { statusCode: res?.statusCode ?? err?.statusCode };
+};
+
+export default ErrorPage;
