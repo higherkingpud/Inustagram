47496a5fe552250d98c71ad00f649d63b319eacf
kijimaru <eddix.ibanez@outlook.jp>
2020-04-04 19:01:11 +0900

    プレビューを追加

--- src/components/object/PhotoCard.tsx
+++ src/components/object/PhotoCard.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'reinspect';
 
 import { Photo } from '../../types';
 
@@ -9,11 +10,26 @@ export default ({
   url,
   title,
 }: Props): React.ReactElement => {
+  const [isPreviewOpen, togglePreviewOpen] = useState(false, `preivew-${title}`);
+  const onClickOverlay = React.useCallback(() => {
+    togglePreviewOpen(false);
+  }, [togglePreviewOpen]);
   return (
     <>
+      {isPreviewOpen && (
+        <>
+          <div className="overlay" onClick={onClickOverlay}/>
+          <img className="preview" src={url} onClick={onClickOverlay}/>
+        </>
+      )}
       <div className="photo-card">
         <h3 className="photo-title">{title}</h3>
-        <img src={url} alt={title}/>
+        <img
+          className="img-cutout"
+          alt={title}
+          onClick={(): void => { togglePreviewOpen(true); }}
+          src={url}
+        />
       </div>
       <style jsx>{`
         .photo-card {
@@ -26,15 +42,35 @@ export default ({
           margin-top: 0;
           margin-bottom: 4px;
         }
-        img {
+        .img-cutout {
           border-radius: 1rem;
           object-fit: cover;
           width: 100%;
           max-height: 24rem;
         }
+        .overlay {
+          z-index: 300;
+        }
+        .preview {
+          top: 0;
+          height: 100vh;
+          margin: auto;
+          object-fit: contain;
+          position: fixed;
+          width: 80vw;
+          z-index: 400;
+        }
+        @media(min-width: 480px) {
+          .preview {
+            transform: translateX(-150px);
+          }
+        }
         @media (max-width: 479px) {
-          img {
+          .img-cutout {
             max-height: 12rem;
+          }
+          .preview {
+            width: 100vw;
           }
         }
       `}</style>
